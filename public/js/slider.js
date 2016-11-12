6aa538e07641bc859204486e6355e248f2b0ae92
Boris Yue <borisyue@yahoo.com>
2016-11-12 19:00:44 +0000

    add slider.js and backstretch and jquery

---
+++ public/js/slider.js
@@ -0,0 +1,7 @@
+// $.backstretch([
+//       "http://dl.dropbox.com/u/515046/www/outside.jpg"
+//     , "http://dl.dropbox.com/u/515046/www/garfield-interior.jpg"
+//     , "http://dl.dropbox.com/u/515046/www/cheers.jpg"
+//   ], {duration: 3000, fade: 750});
+
+$.backstretch("http://videos2.healthination.com/HN_BB_05_EasyCooking_ProRes_739/HN_BB_05_EasyCooking_ProRes_739-img_1280x720.jpg");
