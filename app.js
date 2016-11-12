e7f29a7a198e6e7941062982f8ee5b3939baade9
Boris Yue <borisyue@yahoo.com>
2016-11-12 10:09:20 +0000

    added basic receive and send in app.js

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-var app = require("express"),
-    bodyParser = require("body-parser"),
-    twilio = require("twilio");
+var express = require("express"),
+    bodyParser = require("body-parser");
+    // twilio = require("twilio");
+    
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+
+app.post('/', function(req, res) {
+    var twilio = require('twilio');
+    var twiml = new twilio.TwimlResponse();
+    if (req.body.Body == 'hello') {
+        twiml.message('Hi!');
+    } else if(req.body.Body == 'bye') {
+        twiml.message('Goodbye');
+    } else {
+        twiml.message('No Body param match, Twilio sends this in the request to your server.');
+    }
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("server started");
+});
