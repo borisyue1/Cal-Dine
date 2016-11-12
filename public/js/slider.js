6531d7c3b3974202935d363f426dddaf7f1cfeee
Boris Yue <borisyue@yahoo.com>
2016-11-12 19:26:29 +0000

    almost finished home page

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,7 +1,9 @@
-// $.backstretch([
-//       "http://dl.dropbox.com/u/515046/www/outside.jpg"
-//     , "http://dl.dropbox.com/u/515046/www/garfield-interior.jpg"
-//     , "http://dl.dropbox.com/u/515046/www/cheers.jpg"
-//   ], {duration: 3000, fade: 750});
+$.backstretch([
+      "https://c1.staticflickr.com/6/5299/5547748200_8c369b91a7_b.jpg"
+    , "https://spoonuniversity.com/wp-content/uploads/sites/23/2014/03/People-of-Crossroads-1.jpg"
+    , "https://spoonuniversity.com/wp-content/uploads/sites/23/2015/07/clarkkerr_int-copy-2-1024x709.jpg"
+    , "https://farm4.staticflickr.com/3758/9017389020_e37f7a81f7_b.jpg"
+  ], {duration: 3000, fade: 1200});
 
-$.backstretch("http://videos2.healthination.com/HN_BB_05_EasyCooking_ProRes_739/HN_BB_05_EasyCooking_ProRes_739-img_1280x720.jpg");
+// $.backstretch("http://videos2.healthination.com/HN_BB_05_EasyCooking_ProRes_739/HN_BB_05_EasyCooking_ProRes_739-img_1280x720.jpg");
+// $.backstretch("https://www.pritikin.com/wp/wp-content/uploads/2015/03/healthy-cooking-secrets-20.jpg");
